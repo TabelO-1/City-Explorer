ec9b4bc4a2e6cd03263733aeb84736e47f079daa
masonjar <mason.whitaker79@gmail.com>
2022-01-20 08:04:01 -0800

    Did something

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import './App.css';
+import axios from "axios";
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: ''
-    }
+      city: "",
+    };
   }
   handleChange = (event) => {
     let inputCity = event.target.value;
     console.log(inputCity);
-    this.setState({city: inputCity});
-  }
+    this.setState({ city: inputCity });
+  };
+  getlocation = async (event) => {
+    event.preventDefault();
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
+    let reponse = await axios.get(url);
+    console.log('Reponse: ', reponse.data[0]);
+    this.setState({
+      location: reponse.data[0]
+    }) 
+  };
   render() {
     return (
       <div className="App">
         <h1>City Explorer</h1>
-          <form>
-            Your City:<input name="input" onChange={this.handleChange}></input>
-            <button>Explore!</button>
-          </form>
+        <form onSubmit={this.getlocation}>
+          Your City:<input name="input" onChange={this.handleChange}></input>
+          <button type="submit">Explore!</button>
+        </form>
         <h3>Here's the map for: {this.state.city}</h3>
       </div>
-    )
+    );
   }
 }
 
