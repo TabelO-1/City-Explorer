a69dc5a58cb7ee2efa31956e0f8f9fdf63ad2b13
masonjar <mason.whitaker79@gmail.com>
2022-01-20 08:35:58 -0800

    Working on finishing up card 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import React from "react";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Image from 'react-bootstrap/Image'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       city: "",
+      location: {},
     };
   }
   handleChange = (event) => {
@@ -16,12 +19,12 @@ class App extends React.Component {
   };
   getlocation = async (event) => {
     event.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`;
     let reponse = await axios.get(url);
-    console.log('Reponse: ', reponse.data[0]);
+    console.log("Reponse: ", reponse.data[0]);
     this.setState({
-      location: reponse.data[0]
-    }) 
+      location: reponse.data[0],
+    });
   };
   render() {
     return (
@@ -31,7 +34,13 @@ class App extends React.Component {
           Your City:<input name="input" onChange={this.handleChange}></input>
           <button type="submit">Explore!</button>
         </form>
-        <h3>Here's the map for: {this.state.city}</h3>
+        {this.state.location.display_name && 
+          <Container>
+            <h3>Here's the map for: {this.state.location.display_name}</h3>
+            <p>Lat/Long: {this.state.location.lat}/{this.state.location.lon}</p>
+            <Image/>
+          </Container>
+        }
       </div>
     );
   }
